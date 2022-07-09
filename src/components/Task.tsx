560e340f1d990cc5528a184ab23d955bdfab0fe7
EdenHwang <hgs_0529@naver.com>
2022-07-09 22:43:05 +0900

    first commit

---
+++ src/components/Task.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+
+interface Task {
+  id: number;
+  title: string;
+  state: "TASK_ARCHIVED" | "TASK_PINNED" | "TASK_INBOX" | "TASK_ACTIVE";
+}
+
+interface TaskProps {
+  task: Task;
+  onArchiveTask: (id: number) => void;
+  onPinTask: (id: number) => void;
+}
+
+export default function Task({
+  task: { id, title, state },
+  onArchiveTask,
+  onPinTask,
+}: TaskProps) {
+  return (
+    <div className={`list-item ${state}`}>
+      <label className="checkbox">
+        <input
+          type="checkbox"
+          defaultChecked={state === "TASK_ARCHIVED"}
+          disabled={true}
+          name="checked"
+        />
+        <span
+          className="checkbox-custom"
+          onClick={() => onArchiveTask(id)}
+          id={`archiveTask-${id}`}
+          aria-label={`archiveTask-${id}`}
+        />
+      </label>
+      <div className="title">
+        <input
+          type="text"
+          value={title}
+          readOnly={true}
+          placeholder="Input title"
+        />
+      </div>
+
+      <div className="actions" onClick={(event) => event.stopPropagation()}>
+        {state !== "TASK_ARCHIVED" && (
+          // eslint-disable-next-line jsx-a11y/anchor-is-valid
+          <a onClick={() => onPinTask(id)}>
+            <span
+              className={`icon-star`}
+              id={`pinTask-${id}`}
+              aria-label={`pinTask-${id}`}
+            />
+          </a>
+        )}
+      </div>
+    </div>
+  );
+}
